819af7bcfa480d25059d2f9629e3a1a49764167d
unknown <vivelab@rutanmedellin.org>
2019-03-23 13:19:53 -0500

    Web_Api

---
+++ ultima_clase/ejemplo2.js
@@ -0,0 +1,11 @@
+
+//WEB API
+const item1 = document.createElement('li');
+item1.textContent="Hola mi nombre es Neiver Tapia";
+
+//document.querySelector()
+const lista = document.querySelector("#list");
+
+
+//Destino.appendChild(Nodo a add)
+lista.appendChild(item1);
